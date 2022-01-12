9248a17faaae66e5dd736bab5673201a0a23a290
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-01-12 18:16:56 -0300

    feat: Add List of Objects in Dashboard

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,26 +1,73 @@
-import { useRef } from 'react'
-import { Link } from 'react-router-dom'
-import { useUsuarioLogado } from '../../shared/hooks/UseUsuarioLogado'
+import { useCallback, useState } from "react"
+
+interface IListItem {
+    title: string;
+    isSelected: boolean;
+}
 
 export const Dashboard = () => {
 
-    const counterRef = useRef(0)
+    const [lista, setLista] = useState<IListItem[]>([])
 
-    const {nomeDoUsuario, logout} = useUsuarioLogado()
+    const handleInputKeyDown : React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+        if(e.key === 'Enter'){
+            if(e.currentTarget.value.trim().length === 0) return
+
+            const value = e.currentTarget.value
+
+            e.currentTarget.value = ''
+
+            setLista((oldLista)=>{
+
+                if(oldLista.some((listItem) => listItem.title === value)) return oldLista
+
+                return [
+                    ...oldLista, 
+                    {
+                        title: value,
+                        isSelected: false
+                    }
+                ]
+            })
+        }
+    }, [])
 
     return (
         <div>
-            <p>Dashboard</p>
+            <p>Lista</p>
 
-            <p>{nomeDoUsuario}</p>
-            
-            <p>Contador: {counterRef.current}</p>
-            
-            <button onClick={()=> counterRef.current++}>Somar</button>
-            <button onClick={()=> console.log(counterRef.current)}>Console</button>
-            <button onClick={logout}>Logout</button>
+            <input  
+                onKeyDown={handleInputKeyDown}
+            />
 
-            <Link to="/entrar">Login</Link>
+            <p>{lista.filter((listItem) => listItem.isSelected).length}</p>
+
+            <ul>
+                {lista.map((listItem)=>{
+                    return <li key={listItem.title}>
+                        <input 
+                            type="checkbox" 
+                            checked = {listItem.isSelected}
+                            onChange={()=>{
+                                setLista((oldLista) => {
+                                    return oldLista.map((oldListItem)=>{
+
+                                        const newIsSelected = oldListItem.title === listItem.title 
+                                        ? !oldListItem.isSelected
+                                        : oldListItem.isSelected
+
+                                        return {
+                                            ...oldListItem,
+                                            isSelected: newIsSelected
+                                        }
+                                    })
+                                })
+                            }}
+                        />
+                        {listItem.title}
+                    </li>
+                })}
+            </ul>
         </div>
     )
 }

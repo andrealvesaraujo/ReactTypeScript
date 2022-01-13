bb8bc999a53ee04303b5cd8fac4a156e4c711f04
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-01-13 16:01:37 -0300

    feat: Update a List Item with API

--- src/app/shared/services/api/tarefas/TarefasServices.ts
+++ src/app/shared/services/api/tarefas/TarefasServices.ts
@@ -34,7 +34,7 @@ const create = async (dataToCreate: Omit<ITarefa, 'id'>): Promise<ITarefa | ApiE
     }
 }
 
-const updateById = async (id: string, dataToUpdate: ITarefa): Promise<ITarefa | ApiException> => {
+const updateById = async (id: number, dataToUpdate: ITarefa): Promise<ITarefa | ApiException> => {
     try {
         const { data } = await Api().put(`/tarefas/${id}`, dataToUpdate) 
         return data
@@ -43,7 +43,7 @@ const updateById = async (id: string, dataToUpdate: ITarefa): Promise<ITarefa | 
     }
 }
 
-const deleteById = async (id:string): Promise<undefined | ApiException> => {
+const deleteById = async (id:number): Promise<undefined | ApiException> => {
     try {
         await Api().delete(`/tarefas/${id}`) 
         return undefined
